ca48e447b136e431c66e857eb0d3cdaf66288bca
nicolasgarpov <nicolasgarpov64@gmail.com>
2023-10-24 12:15:04 -0500

    Borrar test duplicados y solucion 7-8

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const resultado1 = parseFloat(((fahrenheit - 32) / 1.8).toFixed(1));
+  console.log(resultado1);
+  return resultado1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const resultado2 = parseFloat(((celsius * 1.8) + 32).toFixed(1));
+  console.log(resultado2);
+  return resultado2;
 };
+
+// Ejemplos de uso
+const temperaturaCelsius = convertToCelsius(68);
+const temperaturaFahrenheit = convertToFahrenheit(20);
+
+console.log(temperaturaCelsius); // Resultado como número
+console.log(temperaturaFahrenheit); // Resultado como número
+
+
 
 // Do not edit below this line
 module.exports = {
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let resultado = 1;
+  for (let i = 1; i <= n; i++) {
+    resultado *= i;
+  }
+  return resultado;
 };
+
 
 // Do not edit below this line
 module.exports = {
